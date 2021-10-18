d06dde1ba5045efdfdd96e122cca68b8bacdc71e
349273532 <59988006+349273532@users.noreply.github.com>
2021-10-18 00:19:00 -0400

    Update YuVictorMazeAssignment.java

--- YuVictorMazeAssignment.java
+++ YuVictorMazeAssignment.java
@@ -15,7 +15,7 @@ class YuVictorMazeAssignment{
    * @param fileName The name of the file
    * @param locationSave The array that will store the location of the start
    */
-  public static void fileToArray(char[][] Maze, String fileName, int[] locationSave){
+  public static void fileToArray(char[][] maze, String fileName, int[] locationSave){
     BufferedReader reader = null;
     String strCurrentLine;
     int row = 0; 
@@ -24,7 +24,7 @@ class YuVictorMazeAssignment{
     try{reader = new BufferedReader(new FileReader(fileName));
       while ((strCurrentLine = reader.readLine()) != null) {
         for (int i = 0; i < strCurrentLine.length(); i++) {
-          Maze[row][i] = strCurrentLine.charAt(i);
+          maze[row][i] = strCurrentLine.charAt(i);
           if(0 == Character.compare(strCurrentLine.charAt(i), 'S')){
             locationSave[0] = row;
             locationSave[1] = i;
@@ -54,7 +54,19 @@ class YuVictorMazeAssignment{
   }
 
   /**
-   * Using RECURSION
+   * Using RECURSION Find the path of the solution if there is any and sets the path in the finished array
+   * Checks if the current location is out of bounds
+   * Marks the location if it is in bounds and is open
+   * 
+   * Tries to move in a direction until it is able to 
+   * If I can't move anymore it will go back to the last location and look for another location
+   * If there is no movement and it has exhausted every movement it returns false
+   * 
+   * If there is another direction open in the last location it will continue moving from that location
+   * When it reaches the end it will start returning true and because it won't move anymore because it's already hit the goal
+   * 
+   * Sets the current locations for the finished path as '+' in the finished array
+   * 
    * @param finishedMaze This is where the finished maze will be added to
    * @param mazeLayout This is taking in the maze layout and will be used to find the path
    * @param row Starting row 
@@ -73,21 +85,13 @@ class YuVictorMazeAssignment{
     }
 
 
-    if('.' == mazeLayout[row][column] || 'G' == mazeLayout[row][column] || 'S' == mazeLayout[row][column]){//Checks if the location is open or not
+    if('.' == mazeLayout[row][column] || 'G' == mazeLayout[row][column] || 'S' == mazeLayout[row][column]){//Checks if the location is open
       if('G' == mazeLayout[row][column]){//Returns True for the final movement
         return true;
       }
       mazeLayout[row][column] = '*';//Marks the locations that its' been
-      /**
-       * All the movement
-       * When one reaches the end it will return true and in a cascade all of them will start returning true
-       * and adding the path to the finished array
-       * causing the overall method to return true
-       * 
-       * If all the movements are blocked it will return false until it can move 
-       * If in all cases it can't move it will return false overall
-       */
 
+      //all the movement
       moved = findPath(finishedMaze, mazeLayout, row-1, column, length, width);//up
       if(!moved){//down
         moved = findPath(finishedMaze, mazeLayout, row+1, column, length, width);
@@ -103,8 +107,11 @@ class YuVictorMazeAssignment{
     if (moved){//If it is finished moving it will put a +
        finishedMaze[row][column] = '+';
     }
-    return moved;
+    return moved;//will return true or false based of if it reaches the end or not
   }
+
+
+
   //------------------------Main Program------------------------\\
   public static void main(String[] args) throws IOException{
 
@@ -146,6 +153,8 @@ class YuVictorMazeAssignment{
       }
     }
 
+    //------------------------Array Setup------------------------\\
+
     //Creating arrays and filling arrays
     char[][] mazeLayout = new char[length][width];
     char[][] finishedMaze = new char[length][width];
@@ -158,21 +167,22 @@ class YuVictorMazeAssignment{
     //------------------------Maze Path Finding------------------------\\
 
 
-    //Printing out the maze before the program
-    System.out.println("Printing the Maze");
+    //Printing out the maze before the solve
+    System.out.println("Printing the Maze:");
     printArray(mazeLayout);
 
-    //Solving the maze using Recursion  
+    //Solving the maze using Recursion function returns true is there is a solution
     if(findPath(finishedMaze, mazeLayout, locationSave[0], locationSave[1], length-1, width-1)){
       //Brings the S back in to the maze
       finishedMaze[locationSave[0]][locationSave[1]] = 'S';
-      //Printing out the maze after the program
-      System.out.println("There is a solution");
-      System.out.println("Printing out the Solution");
+      //Printing out the maze the solve
+      System.out.println("There is a solution \nPrinting out the Solution:");
       printArray(finishedMaze);
     }
+
+
     else{//If the is no solution
-      System.out.println("There is no solution for this maze");
+      System.out.println("There is no solution for this Maze");
     }
 
     //closes the open readers
